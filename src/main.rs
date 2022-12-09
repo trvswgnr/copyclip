9999c225fa47931cc7a19ceeed8aeabfc2b0975f
Travis Wagner <travis.wagner@umusic.com>
2022-12-09 01:22:58 -0500

    revert test

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@ use clipboard::{ClipboardContext, ClipboardProvider};
 use std::{
     error::Error,
     io::{self, Read},
-    sync::Mutex,
 };
-
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref CTX: Mutex<ClipboardContext> = Mutex::new(ClipboardProvider::new().unwrap());
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    CTX.lock().unwrap().set_contents(input)?;
+    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+    ctx.set_contents(input)?;
 
     Ok(())
 }
@@ -26,21 +20,10 @@ mod tests {
 
     #[test]
     fn test() -> Result<(), Box<dyn Error>> {
-        // start a thread that runs the command `echo "hello world" | cargo run`
-        let mut child = std::process::Command::new("echo")
-            .arg("hello world")
-            .arg("|")
-            .arg("cargo")
-            .arg("run")
-            .spawn()?;
-        // kill the child process after 200ms
-        std::thread::sleep(std::time::Duration::from_millis(200));
-        // wait for the child process to exit
-        child.wait()?;
-        // get the clipboard contents
-        let mut _ctx = CTX.lock().unwrap();
-        let contents = _ctx.get_contents()?;
-        assert_eq!(contents, "hello world");
+        let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+        ctx.set_contents("test".to_string())?;
+        assert_eq!(ctx.get_contents()?, "test");
+
         Ok(())
     }
 }
